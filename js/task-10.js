a1e806bd63da3fa76b3ee3c3e8f50ff81dbd1037
Albina Bondar <albinabondar2019@gmail.com>
2022-08-08 19:53:13 +0300

    save

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,24 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBtn = document.querySelector('button[data-create]')
+const destroyBtn = document.querySelector('button[data-destroy]')
+const input = document.querySelector("input[type='number']")
+const boxes = document.querySelector("#boxes")
+
+
+createBtn.addEventListener("click", () => {
+  
+})
+
+function createBoxes(amount) {
+
+  for (let i = 0; i < amount.length; i++) {
+    document.createElement("div[i]");
+    console.log(div);
+  }
+  
+  
+
+}
